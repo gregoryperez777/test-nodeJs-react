5dd5ed49271ca6738f4f66b3ee9204d4fb6c17ec
Gregory Perez <gregoryperezfigueredo@gmail.com>
2019-01-05 15:23:50 -0400

    it started with the layout of the login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+	Container, Col, Form, FormGroup, Label, Input, Button, Row
+} from 'reactstrap';
 import './App.css';
 
 const App = () => (
-	<div className='App'>
-		<header className='App-header'>
-			<img src={logo} className='App-logo' alt='logo' />
-			<p>
-				Edit <code>src/App.js</code> and save to reload.
-			</p>
-			<a
-				className='App-link'
-				href='https://reactjs.org'
-				target='_blank'
-				rel='noopener noreferrer'
-			>
-				Learn React
-			</a>
-		</header>
+	<div className='Align-login'>
+		<Container className='App'>
+			<h2>Tres en Linea</h2>
+			<Form className='form'>
+				<Col>
+					<FormGroup>
+						<Label>Email</Label>
+						<Input
+							type='email'
+							name='email'
+							id='Email'
+							placeholder='myemail@email.com'
+						/>
+					</FormGroup>
+				</Col>
+				<Col>
+					<FormGroup>
+						<Label for='Password'>Password</Label>
+						<Input
+							type='password'
+							name='password'
+							id='Password'
+							placeholder='********'
+						/>
+					</FormGroup>
+				</Col>
+				<Row>
+					<Col>
+						<Button color='success' className='btn btn-primary btn-lg btn-block'>
+							Submit
+						</Button>
+					</Col>
+					<Col>
+						<Button color='primary' className='btn btn-primary btn-lg btn-block'>
+							Register
+						</Button>
+					</Col>
+				</Row>
+			</Form>
+		</Container>
 	</div>
 );
 
