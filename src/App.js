8b3d3fe506a7b663ea759ea9e3ea99d3b54733b8
Gregory Perez <gregoryperezfigueredo@gmail.com>
2019-01-08 12:10:14 -0400

    the automatic desologation functionality was added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import IdleTimer from 'react-idle-timer';
+import { logout } from './actions/actionsLogin';
 import Login from './components/Login';
 import './App.css';
 
-const App = ({ auth }) => (
+const App = ({ auth, actionLogout }) => (
 	<div>
 		{!auth && <Login />}
 
-		{auth && <h1> hola </h1>}
+		{auth && (
+			<IdleTimer element={document} onIdle={() => actionLogout()} timeout={1000 * 60 * 15}>
+				<h1> hola </h1>
+			</IdleTimer>
+		)}
 	</div>
 );
 
 App.propTypes = {
 	auth: PropTypes.bool.isRequired,
+	actionLogout: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
 	auth: state.ReducerLogin.auth,
 });
 
+const mapDispatchToProps = dispatch => ({
+	actionLogout: () => dispatch(logout()),
+});
+
 export default connect(
 	mapStateToProps,
-	null
+	mapDispatchToProps
 )(App);
